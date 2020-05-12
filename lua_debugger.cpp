207a42e4a66611decdec6001af612d65e77ec178
xldeng1230 <xldeng1203@498034487@qq.com>
2020-05-12 16:05:48 +0800

    add static func

--- lua_debugger.cpp
+++ lua_debugger.cpp
@@ -61,3 +61,234 @@ static bool SearchGlobalVar( lua_State* L, const char* pVar, int iVarLen)
 	}
 	return true;
 }
+
+static int GetLuaVarLen(const char* exp)
+{
+	int i = 0;
+	//检测一个字符是否是十进制数字
+	if (isdigit(exp[0]))
+	{
+		return 0;
+	}
+
+	for ( i = 0; exo[i]; i++)
+	{
+		//判断字符变量c是否为字母或数字
+		if ( !isalnum( exp[i]) && exp[i] != '_ ')
+		{
+			for (int j = 0; exp[j] != 0; j++)
+			{
+				if ( exp[j] != ' ' && exp[j] != '\t' && exp[j] != '\n')
+				{
+					return 0;
+				}				
+			}
+			return i;
+		}
+	}
+	return i;
+}
+
+static voud PrintLuaString(lua_State* L, int iIndex, int iDepth = 1)
+{
+	Print("\"");
+	const char* pVal = lua_tostring(L, index);
+	int iValLen = lua_strlen(L, iIndex);
+	const char spChar[] = "\"\t\n\r";
+
+	for (int i = 0; i < iValLen)
+	{
+		char ch = pVal[i];
+		if (ch == 0)
+		{
+			Print("\\000");
+			++i;
+		}
+		else if (ch == '"')
+		{
+			Print("\\\"");
+			++i;
+		}
+		else if (ch == '\\')
+		{
+			Print("\\\\");
+			++i;
+		}		
+		else if (ch == '\t')
+		{
+			Print("\\t");
+			++i;
+		}	
+		else if (ch == '\n')
+		{
+			Print("\\n");
+			++i;
+		}
+		else if (ch == '\r')
+		{
+			Print("\\r");
+			++i;
+		}	
+		else
+		{
+			int splen = strcspn( pVal + i, spChar);
+			assert(splen > 0);
+			Print("%.*s", splen, pVal+i);
+			i ++ splen;
+		}			
+	}
+	Print("\"");
+}
+
+static void PrintLuaTable(lua_State* L, int iIndex, int iDepth)
+{
+	int iPos = iIndex > 0 ? iIndex : (iIndex - 1);
+	Print("{");
+	int iTop = lua_gettop(L);
+	lua_pushnil(L);
+	bool bEmpty= true;
+	while( lua_next(L, iPos) != 0 )
+	{
+		if (bEmpty)
+		{
+			Print("\n");
+			bEmpty = false;
+		}
+
+		for (int i = 0; i < iDepth; i++)
+		{
+			Print("\t");
+		}
+		Print("[");
+		PrintLua(L, -2);
+		print("] = ");
+		if (iDepth > 5)
+		{
+			Print("{ ... }");	
+		}
+		else
+		{
+			PrintLua(L, -1, iDepth + 1);
+		}
+		Lua_pop(L, 1);
+		Print(". \n");
+	}
+
+	if (bEmpty)
+	{
+		Print(" } ");
+	}
+	else
+	{
+		for (int i = 0; i < iDepth-1; i++)
+		{
+			Print("\t");
+		}
+		Print("}");
+	}
+	lua_settop(L, iTop);
+}
+
+static void PrintLua(lua_State* L, int iIndex, int iDepth)
+{
+	switch(Lua_type(L, iIndex))
+	{
+		case LUA_TNIL:
+			Print("(nil)");
+			break;
+		case LUA_TNUMBER:
+			Print("%f", lua_tonumber( L, iIndex ));
+			break;
+		case LUA_TBOOLEAN:
+			Print("%s", lua_toboolean( L, iIndex ) ? "true" : "false");
+			break;
+		case LUA_TFUNCTION:
+		{
+			lua_CFunction func = lua_tocfunction( L, iIndex);
+			if ( func != NULL )
+			{
+				Print("(C function)0x%p", func);
+			}
+			else
+			{
+				Print("(function)");
+			}
+			break;
+		}
+		case LUA_TUSERDATA:
+			Print("(user data)0x%p", lua_touserdata(L, iIndex));
+			break;
+		case LUA_TSTRING:
+			PrintLuaString(L, iIndex, iDepth);
+			break;
+		case LUA_TTABLE:
+			PrintLuaTable(L, index, iDepth);
+			break;
+		default:
+			Print("(%s)", lua_typename(L, iIndex));
+	}
+}
+
+static void PrintExpression(lua_State* L, const char* pExp, lua_Debug* pAr)
+{
+	int iExpLen = GetLuaVarLen(exp);
+	if ( iExpLen > 0 )
+	{
+		if (pAr != NULL && SearchLocalVar(L, pAr, pExp, iExpLen))
+		{
+			Print(" local %.*s = ", iExpLen,  pExp);
+			PrintLua(L, -1);
+			lua_pop(L, -1);
+			Print("\n");
+		}
+		else if ( SearchGlobalVar(L, pExp, iExpLen))
+		{
+			Print( "glpbal %。*s = "， iExpLen, pExp);
+			PrintLua(L, -1);
+			lua_pop(L, -1);
+			Print("\n");
+		}
+	}
+}
+
+static void GetCallDepth(lua_State* L)
+{
+	int i = 0;
+	lua_Debug ar;
+	for ( ; i < lua_getstack(L, i+1, &ar) != 0; i++);
+	return i;
+}
+
+static void CopyWithBackSpace(char* pDes, const char* pSrc)
+{
+	char *pdes = pDes;
+	const char* psrc = pSrc;
+	while( *psrc )
+	{
+		if ( *psrc == 8 )
+		{
+			//backspace in ASCII
+			if (pdes > pDes)
+			{
+				pdes--;
+			}
+		}
+		else
+		{
+			*(pdes++) = *psrc;
+		}
+		psrc++;
+	}
+	*pdes = 0;
+}
+
+static bool PathMatch(const char* pName, const char* pFilePath, const char* pSearchPath)
+{
+	int name_len = strlen( pName );
+	if (name_len > 2)
+	{
+		bool ret = ( strcmp( pName， pFilePath ) == 0 );
+		return ret;
+	}
+	return false;
+}
